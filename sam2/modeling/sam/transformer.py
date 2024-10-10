bfe122b372bd71b81120ede76911cb47ee182784
Gabriel Armas Aranibar <gabriel.armas@mail.concordia.ca>
2024-10-10 09:44:55 -0400

    Update transformer.py

--- sam2/modeling/sam/transformer.py
+++ sam2/modeling/sam/transformer.py
@@ -19,8 +19,12 @@ from sam2.modeling.sam2_utils import MLP
 from sam2.utils.misc import get_sdpa_settings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
-OLD_GPU, USE_FLASH_ATTN, MATH_KERNEL_ON = get_sdpa_settings()
 
+#OLD_GPU, USE_FLASH_ATTN, MATH_KERNEL_ON = get_sdpa_settings()
+
+USE_FLASH_ATTN = False
+MATH_KERNEL_ON = True
+OLD_GPU = True
 
 class TwoWayTransformer(nn.Module):
     def __init__(
